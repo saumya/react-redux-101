d9157acd24d62d2ef1d2b569b1402c88cc6900b5
saumya <2saumyaray@gmail.com>
2016-04-19 19:51:37 +0530

    added Provider from react-redux library

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 /**
  * Application Entry
  */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Counter from './components/Counter.react'
 import counter from './reducers'
 
@@ -12,11 +14,13 @@ const appDomNode = document.getElementById('react-app')
 
 function render() {
   ReactDOM.render(
+    <Provider store={store}>
     <Counter
       value={store.getState()}
       onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
       onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />,
+    />
+    </Provider>,
     appDomNode
   )
 }
